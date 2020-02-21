1fb44b1d6be93529281d23e43c6c4b5f2adfc11b
DavidMachajewski <44371378+DavidMachajewski@users.noreply.github.com>
2020-02-21 02:21:08 +0100

    Added hardcoded Sudoku edge coordinates, the midpoint and getter functions.

--- app/src/main/java/sse/goethe/arsudoku/ml/Recognition.kt
+++ app/src/main/java/sse/goethe/arsudoku/ml/Recognition.kt
@@ -11,14 +11,19 @@ import android.graphics.Bitmap
 
 class Recognition {
 
+    /*+++++++++++++++++++++++++++++++++++++++
+    * Variables and Values
+    *++++++++++++++++++++++++++++++++++++++++ */
     /* Sudokus 4 edge coordinates */
-    private var edgeCoordinates: Array<Array<Int>>
-        get() { return edgeCoordinates }
+    private var sudokuEdgeCoordinates: Array<Array<Int>>
+        get() { return sudokuEdgeCoordinates }
 
-    private lateinit var sudokuMid: Array<Int>
+    private var sudokuMidCoordinates: Array<Int>
+        get() { return sudokuMidCoordinates}
 
     /* 81 dim array with classes 0..19 */
-    private var digitPredictions: Array<Int>
+    private var sudokuPredictedDigits: Array<Int>
+        get() { return sudokuMidCoordinates }
 
     /* This cropped Sudoku blocks will go to the classifier */
     private lateinit var croppedSudokuBlocks: Array<Bitmap>
@@ -28,10 +33,59 @@ class Recognition {
     // Array of cropped Bitmaps
 
     init {
-        edgeCoordinates = arrayOf(arrayOf(0,0), arrayOf(0,0), arrayOf(0,0), arrayOf(0,0))
-        digitPredictions = Array(81) {i -> 0}
+        // x1 top left corner, x2 top right corner, x3 bottom left ...
+        sudokuEdgeCoordinates = arrayOf(    arrayOf(700, 2000), arrayOf(1200,2000),
+                                            arrayOf(700, 1500), arrayOf(1200, 1500) )
+
+        sudokuMidCoordinates = arrayOf( 950, 1750 )
+
+        /* 0 = empty field, 1-9 machine written, 10 - 18 hand written */
+        sudokuPredictedDigits = arrayOf(    5, 0, 0, 3, 0, 1, 0, 0, 7,
+                                            0, 1, 0, 4, 0, 6, 0, 9, 0,
+                                            0, 0, 8, 0, 5, 0, 4, 0, 0,
+                                            1, 7, 0, 0, 0, 0, 0, 5, 9,
+                                            0, 0, 6, 0, 0, 0, 7, 0, 0,
+                                            4, 2, 0, 0, 0, 0, 0, 8, 3,
+                                            0, 0, 4, 0, 2, 0, 3, 0, 0,
+                                            0, 8, 0, 7, 0, 5, 0, 2, 0,
+                                            2, 0, 0, 9, 0, 4, 0, 0, 5   )
+
 
         // Initialize Interpreter from DigitClassifier
+    }
+
+    /** ++++++++++++++++++++++++++++++++++++
+     * CLASS FUNCTIONS
+     +++++++++++++++++++++++++++++++++++++++*/
+
+    private fun cropSudoku() {
+        // Get Sudoku from Stream
+        // convert into Bitmap
+        // and crop with some OCV functions
+        // croppedSudokuBlocks = ...
+    }
+
+    private fun calculateSudokuMid() {
+        // take the 4 edge coordinates
+        // and calculate the midpoint
+        // (x1, y1), (x2, y2), (x3, y3), (x4, y4)
+        // (x2 - x1) / 2 and
+        // (y1 - y3) / 2
+        // sudokuMidCoordinates
+    }
+
+    private fun calculateSudokuDigitCells() {
+        // just calculates the array position of
+        // a digit to the 2 dim cell positions
+        // within the sudoku 81x81 field
+
+        // use modulo: e.g.
+        // 10 mod 9 = 1
+        // 11 mod 9 = 2 ... for column number
+        // for row number just divide
+        // the array position by 9,
+        // and if it is floating point number
+        // then round up.
     }
 
 }
