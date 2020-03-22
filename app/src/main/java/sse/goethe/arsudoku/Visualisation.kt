ed10f5bb147041f8455c788f21ec3ae5e234eb5f
Kelviiiin <Kelviiiin@users.noreply.github.com>
2020-03-22 23:39:52 +0100

    Adding Rotation Function
    Adding function rotateMat(...)

--- app/src/main/java/sse/goethe/arsudoku/Visualisation.kt
+++ app/src/main/java/sse/goethe/arsudoku/Visualisation.kt
@@ -1,6 +1,7 @@
 package sse.goethe.arsudoku
 
 import org.opencv.core.*
+import org.opencv.core.Core.LINE_8
 import org.opencv.core.Core.inRange
 import org.opencv.imgproc.Imgproc
 import sse.goethe.arsudoku.ml.Recognition
@@ -13,6 +14,8 @@ class Visualisation(recognition: Recognition) {
     // colums and row size
     private val TOTAL_ROWS = 9
     private val TOTAL_COLS = 9
+
+    private val TEXT_COLOR = Scalar(255.0, 255.0, 255.0, 0.0) // white = (255,255,255)
 
     // connection to Recognition and ComputerVision
     private var recognition: Recognition = recognition
@@ -28,6 +31,7 @@ class Visualisation(recognition: Recognition) {
     private lateinit var mask : Mat
     private lateinit var outputMat : Mat
 
+
     /**
      *   Function startVisualisation (input is the current video stream as Mat)
      *       - starts the visualisation if the sudoku is found and the input isn't null
@@ -36,11 +40,11 @@ class Visualisation(recognition: Recognition) {
     fun runVisualisation(inputFrame: Mat) : Mat {
 
         return if (getInput(inputFrame)) {
-            //renderDigits()
+            renderDigits()
             //perspectiveTransform()
             //createMask()
             //mergeMat()
-            testPlot(inputMat!!)
+            testPlot(sudokuMat!!)
             outputMat
         }
         else inputFrame
@@ -90,11 +94,29 @@ class Visualisation(recognition: Recognition) {
                                     Point(x,y),
                             3,
                             1.0,
-                                    Scalar(0.0,0.0,0.0))
+                                     TEXT_COLOR,
+                            1,
+                                    LINE_8,
+                        true)
                 }
             }
         }
-        sudokuMat = canvas
+        sudokuMat = rotateMat(canvas)
+    }
+
+    /**
+     *  Function rotateMat (input: Mat)
+     *      - rotate Mat by (angle =) 90 degrees
+     *
+     *      https://stackoverflow.com/questions/15043152/rotate-opencv-matrix-by-90-180-270-degrees
+     */
+    private fun rotateMat (input : Mat) : Mat {
+        val angle = 270.0 // degrees
+        val centerPoint : Point = Point(input.cols()/2.0, input.rows()/2.0)
+        val rotMat : Mat = Imgproc.getRotationMatrix2D(centerPoint, angle, 1.0)
+        var dst : Mat = Mat.zeros(input.cols(), input.rows(), input.type())
+        Imgproc.warpAffine(input, dst, rotMat, input.size(), Imgproc.INTER_LINEAR, Core.BORDER_CONSTANT)
+        return dst
     }
 
     /**
@@ -123,7 +145,7 @@ class Visualisation(recognition: Recognition) {
      *  Function createMask
      *      - creates mask
      *
-     *  https://stackoverflow.com/questions/45131216/opencv-overlay-two-mat-drawings-not-images-with-transparency
+     *      https://stackoverflow.com/questions/45131216/opencv-overlay-two-mat-drawings-not-images-with-transparency
      */
     private fun createMask () {
 
